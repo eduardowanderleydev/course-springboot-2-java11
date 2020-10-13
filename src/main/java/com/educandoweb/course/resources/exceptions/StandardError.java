e4ab0d116c33571ddd3acc2e3a60c9d0468b7bdd
Eduardo Wanderley <eduardobraz.junior@ucsal.edu.br>
2020-10-13 20:35:08 -0300

    Exception - Delete

--- src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
@@ -7,14 +7,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")	
 	private Instant timestamp;
 	private Integer status;
 	private String error;
 	private String message;
 	private String path;
-
+	
 	public StandardError() {
 	}
 
@@ -66,5 +66,4 @@ public class StandardError implements Serializable {
 	public void setPath(String path) {
 		this.path = path;
 	}
-
 }
