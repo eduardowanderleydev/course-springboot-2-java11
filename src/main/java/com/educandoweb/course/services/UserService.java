d6a1bda9ca4b48f41c5639c9a839583c80931f6e
Eduardo Wanderley <eduardobraz.junior@ucsal.edu.br>
2020-10-13 20:01:11 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -32,5 +32,15 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
-	
+	public User update(long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
